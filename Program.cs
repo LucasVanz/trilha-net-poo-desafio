92c77fb274fdb1ff0b11cba1376da536de5edbdb
Lucas Vanzella <lucasvanzellap2@gmail.com>
2023-01-21 13:28:32 -0300

    sim

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Iphone: ");
+Iphone iphone = new Iphone(numero:"1", modelo:"a", imei: "1", memoria: 9);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Youtube");
+
+Console.WriteLine("\n");
+Console.WriteLine("Nokia: ");
+Nokia nokia = new Nokia(numero:"2", modelo:"b", imei: "2", memoria: 15);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Binance");
+
